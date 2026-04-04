e5393d76aa893bb6f4930ca4aec0a64af6cc4a83
Chatbubble445 <virraajj1@gmail.com>
2026-04-05 01:56:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,75 +9,46 @@ const io = socketIo(server);
 app.use(express.static("public"));
 
 let users = {};
-let messages = [];
 
 io.on("connection", (socket) => {
-  users[socket.id] = {
-    name: "Guest",
-    room: "global"
-  };
 
   socket.on("join", ({ name }) => {
-    users[socket.id].name = name || "Guest";
-    socket.join("global");
+    users[socket.id] = { name };
 
-    messages.forEach((m) => {
-      if (m.type === "image") {
-        socket.emit("image", m);
-      } else {
-        socket.emit("message", m);
-      }
-    });
-
+    io.emit("system", { text: name + " joined" });
     io.emit("users", users);
-    io.emit("system", {
-      text: users[socket.id].name + " joined"
-    });
   });
 
   socket.on("message", (msg) => {
     const user = users[socket.id];
-    if (!msg || !user) return;
+    if (!user) return;
 
-    const data = {
+    io.emit("message", {
       user: user.name,
       text: msg
-    };
-
-    messages.push(data);
-    io.emit("message", data);
+    });
   });
 
   socket.on("image", (img) => {
     const user = users[socket.id];
-    if (!img || !user) return;
+    if (!user) return;
 
-    const data = {
-      type: "image",
+    io.emit("image", {
       user: user.name,
       src: img
-    };
-
-    messages.push(data);
-    io.emit("image", data);
-  });
-
-  socket.on("typing", (name) => {
-    socket.broadcast.emit("typing", name);
+    });
   });
 
   socket.on("disconnect", () => {
-    const user = users[socket.id];
-    if (user) {
+    if (users[socket.id]) {
       io.emit("system", {
-        text: user.name + " left"
+        text: users[socket.id].name + " left"
       });
     }
     delete users[socket.id];
     io.emit("users", users);
   });
+
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
+server.listen(process.env.PORT || 3000);
