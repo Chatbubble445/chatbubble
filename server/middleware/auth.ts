979231cce9fe79584ea4c32af2358efd10e9aca8
Chatbubble445 <virraajj1@gmail.com>
2026-04-05 16:06:00 +0530

    Update auth.ts

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,1 +1,8 @@
-
+export const isOwner = (req: any, res: any, next: any) => {
+    const { username } = req.body;
+    if (username === 'Lord_lucifer') {
+        next();
+    } else {
+        res.status(403).json({ error: "Access Denied: Sirf Owner hi ye kar sakta hai!" });
+    }
+};
