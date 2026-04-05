9ce769437b69042707629d18294e44c8f2e3b80e
Chatbubble445 <virraajj1@gmail.com>
2026-04-05 16:07:44 +0530

    Update ChatBox.tsx

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -1,1 +1,15 @@
+import React from 'react';
 
+const ChatBox = ({ messages }: { messages: any[] }) => {
+  return (
+    <div style={{ height: '400px', overflowY: 'scroll', background: '#1e1e1e', padding: '10px' }}>
+      {messages.map((m, i) => (
+        <p key={i}>
+          <b style={{ color: m.user === 'Lord_lucifer' ? 'red' : 'cyan' }}>{m.user}:</b> {m.text}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default ChatBox;
